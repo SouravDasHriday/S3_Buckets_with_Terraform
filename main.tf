8e1f659abc74470b9afe176409e35ac608896b00
S.D. Hriday <SD@Mac-mini.local>
2026-05-25 21:26:57 +0600

    1st commit: create main.tf file to create a s3 bucket & upload a image inside bucket with terraform

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "nextwork-unique-bucket-sourav-12345" # Make sure this bucket name is globally unique by typing a long random number
+
+  tags = {
+    Project = "Create a S3 bucket with Terraform"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
@@ -14,3 +18,10 @@ resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
   block_public_policy     = true
   restrict_public_buckets = true
 }
+# upload a image in S3 bucket
+resource "aws_s3_object" "image" {
+  bucket = aws_s3_bucket.my_bucket.id # Reference the bucket ID
+  key    = "image.png" # Path in the bucket
+  source = "image.png" # Local file path
+}
+
